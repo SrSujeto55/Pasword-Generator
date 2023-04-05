bea4132313fe631214c7e10422cbc5ae060e0d8e
EmilianoLP <EmilianoLP@ciencias.unam.mx>
2023-04-04 22:19:26 -0600

    JsLogic
    in this commit i added the Javascript functionality to the page
    now it generates actual Pseudo-Random passwords.
    I change some bugs in the CSS in order to beauty the page.

--- src/JS/index.js
+++ src/JS/index.js
@@ -2,3 +2,18 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$",
 "%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+
+function generate() {
+    let fstGen = document.querySelector("#pass_1")
+    let sndGen = document.querySelector("#pass_2")
+    let firtsPass = ""
+    let sencondPass = ""
+
+    for(let i = 0; i < 15; i++){
+        firtsPass += characters[Math.floor(Math.random()*91)]
+        sencondPass += characters[Math.floor(Math.random()*91)]
+    }
+    
+    fstGen.textContent = firtsPass
+    sndGen.textContent = sencondPass
+}
