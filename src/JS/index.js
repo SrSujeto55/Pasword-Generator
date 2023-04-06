521555942020686861bd317a37b4404f4b1ddf36
EmilianoLP <EmilianoLP@ciencias.unam.mx>
2023-04-06 00:57:51 -0600

    Input Length
    In this commit i added the function to set the lenght
    of the password, with a limit of 50 characters, because everything
    breakes if you go beyond that limit and i don´t know how to fix it...
    yet

--- src/JS/index.js
+++ src/JS/index.js
@@ -5,6 +5,8 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 const charactersLight = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g",
 "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
+let inputLength = 15;
+
 function generate() {
     let checkbox = document.getElementById("specialist");
     let fstGen = document.querySelector("#pass_1");
@@ -13,12 +15,12 @@ function generate() {
     let sencondPass = "";
     
     if(checkbox.checked){
-        for(let i = 0; i < 15; i++){
+        for(let i = 0; i < inputLength; i++){
             firtsPass += characters[Math.floor(Math.random()*91)];
             sencondPass += characters[Math.floor(Math.random()*91)];
         }
     }else{
-        for(let i = 0; i < 15; i++){
+        for(let i = 0; i < inputLength; i++){
             firtsPass += charactersLight[Math.floor(Math.random()*52)];
             sencondPass += charactersLight[Math.floor(Math.random()*52)];
         }
@@ -52,4 +54,24 @@ function outFnc(){
 
     var tooltip = document.getElementById("myTooltip2");
    tooltip.innerHTML = "Copy on clipboard";
+}
+
+function getVal() {
+    var errorText = document.querySelector("#ErrorText");
+    var inputData = document.querySelector("#inputLength").value;
+    inputData = parseInt(inputData);
+
+    if(!Number.isInteger(inputData)){
+        inputData.textContent = "15";
+        console.log("FATAL ERROR, NOT A NUMBER");
+        errorText.textContent = "Not a number. Using default length (15)";
+
+    }else if(inputData > 50){
+        errorText.textContent = "Dangerous length, using MaxLength (50)";
+        inputLength = 50;
+
+    }else{
+        errorText.textContent = "";
+        inputLength = inputData;
+    }
 }
